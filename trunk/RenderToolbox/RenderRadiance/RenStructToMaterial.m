0c5c84ca49854d5b7022748bf735bc1776fb1263
brainard <brainard@cbd9ef29-7d05-0410-a576-914e3c4f8fb0>
2005-11-18 18:07:41 +0000

    Bei's changes for spotlight

--- trunk/RenderToolbox/RenderRadiance/RenStructToMaterial.m
+++ trunk/RenderToolbox/RenderRadiance/RenStructToMaterial.m
@@ -8,6 +8,9 @@ function RenStructToMaterial(mat,object_name,dirName)
 % 2/2/04    bx, pk      Wrote it.
 % 3/5/04    bx, dhb     Change name, add comments, etc.
 
+% 17/11/05  bx,jb       Modified it. added alternative lighting type, spotlight.
+
+
 % Get number of wavelengths
 wavelength_count = length(mat.wavelength);
 
@@ -35,21 +38,46 @@ switch (mat.type)
             cd ..
 		end
     case 'light',
-        output = ['void light ',object_name];
-        output = [output,char(10),'0',char(10),'0',char(10),'3 '];
-        wavelength_count = length( mat.wavelength );
-        
-        for i = 1:wavelength_count; 
-            cd(dirName);
-            wavelength = int2str(mat.wavelength(i));
-            spectrum = num2str(mat.spectrum(i));
-            out_name = [object_name,'_',mat.type,'_',wavelength,'.rad'];
-            fid = fopen(out_name,'w');
-            
-            new_output = [output,spectrum,' ',spectrum,' ',spectrum,char(10)];
-            count = fprintf(fid,'%s',new_output);
-            fclose(fid);
-            cd ..
+        switch (mat.lighttype)
+           case 'area'
+               output = ['void light ',object_name];
+               output = [output,char(10),'0',char(10),'0',char(10),'3 '];
+               wavelength_count = length( mat.wavelength );
+
+               for i = 1:wavelength_count; 
+                   cd(dirName);
+                   wavelength = int2str(mat.wavelength(i));
+                   spectrum = num2str(mat.spectrum(i));
+                   out_name = [object_name,'_',mat.type,'_',wavelength,'.rad'];
+                   fid = fopen(out_name,'w');
+
+                   new_output = [output,spectrum,' ',spectrum,' ',spectrum,char(10)];
+                   count = fprintf(fid,'%s',new_output);
+                   fclose(fid);
+                   cd ..
+               end
+            case 'spot'
+               output = ['void spotlight ',object_name];
+               output = [output,char(10),'0',char(10),'0',char(10),'7 '];
+               wavelength_count = length( mat.wavelength );
+
+               for i = 1:wavelength_count; 
+                   cd(dirName);
+                   wavelength = int2str(mat.wavelength(i));
+                   spectrum = num2str(mat.spectrum(i));
+                   width = num2str(mat.conewidth(i));
+                   xv = num2str(mat.xdirection(i));
+                   yv = num2str(mat.ydirection(i));
+                   zv = num2str(mat.zdirection(i));
+                   
+                   out_name = [object_name,'_',mat.type,'_',wavelength,'.rad'];
+                   fid = fopen(out_name,'w');
+
+                   new_output = [output,spectrum,' ',spectrum,' ',spectrum,' ',width,' ',xv, ' ' ,yv, ' ', zv, ' ' ,char(10)];
+                   count = fprintf(fid,'%s',new_output);
+                   fclose(fid);
+                   cd ..
+               end
         end
     otherwise,
         error(sprintf('Unsupported material type %s entered\n',mat.type));
